ba75e758015e1db56c51864e4928b7b7c3a8d7bc
Siddhartha Basu <biosidd@gmail.com>
2014-12-13 14:23:54 +0000

    Added two action creators

---
+++ app/templates/src/actions/ServerActionCreators.js
@@ -0,0 +1,8 @@
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+var ActionTypes = require('../constants/ActionTypes');
+
+var actions = {
+    //define functions to call the dispatcher
+};
+
+module.exports = actions;
---
+++ app/templates/src/actions/ViewActionCreators.js
@@ -0,0 +1,9 @@
+var api = require('../utils/api');
+
+var actions = {
+    // define functions to call the web api
+    // these functions will be called by react view components
+
+};
+
+module.exports = actions;
