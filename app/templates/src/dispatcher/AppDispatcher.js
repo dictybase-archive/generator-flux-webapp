2f4c32bf1c18469ec0e00e03c8c391e30e3000a2
Siddhartha Basu <biosidd@gmail.com>
2014-12-13 12:03:57 +0000

    Added AppDispatcher template

---
+++ app/templates/src/dispatcher/AppDispatcher.js
@@ -0,0 +1,24 @@
+/*
+ * A singleton that operates as the central hub for application updates.
+ * For more information visit https://facebook.github.io/flux/
+ */
+
+var Dispatcher = require('flux').Dispatcher;
+var assign = require('object-assign');
+
+var AppDispatcher = assign(new Dispatcher(), {
+
+  /**
+   * @param {object} action The details of the action, including the action's
+   * type and additional data coming from the server.
+   */
+  handleServerAction: function(action) {
+    var payload = {
+      source: 'SERVER_ACTION',
+      action
+    };
+    this.dispatch(payload);
+  }
+});
+
+module.exports = AppDispatcher;
