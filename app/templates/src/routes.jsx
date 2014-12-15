c06a8ede15f025256754dcfe99eaeed1a0ab16d0
Siddhartha Basu <biosidd@gmail.com>
2014-12-15 21:16:02 +0000

    Added router definitions

---
+++ app/templates/src/routes.jsx
@@ -0,0 +1,10 @@
+var Router = require('react-router');
+var {DefaultRoute} = Router;
+
+var routes = (
+    <Route handler={}>
+    // define your routes here
+    </Route>
+);
+
+module.exports = routes;
