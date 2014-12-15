ded28df18b615299ba3d7038b569bf11b20e75a7
Siddhartha Basu <biosidd@gmail.com>
2014-12-15 21:09:23 +0000

    Added gulp build system

---
+++ app/templates/gulp/dev_server.js
@@ -0,0 +1,48 @@
+var webpack = require("webpack");
+var WebpackDevServer = require("webpack-dev-server");
+var webpackDevConfig = require("../webpack-dev.config.js");
+var webpackDev = require('../webpack.config.js');
+var os = require('os');
+var gulp = require("gulp");
+var gutil = require("gulp-util");
+var ghpages = require('gh-pages');
+var path = require('path');
+
+var ipAddr = os.networkInterfaces().eth0[0].address;
+gulp.task("webpack-dev-server", function(callback) {
+	// modify some webpack config options
+	var myConfig = Object.create(webpackDevConfig);
+	// Start a webpack-dev-server
+	new WebpackDevServer(webpack(myConfig), {
+        publicPath: myConfig.output.publicPath,
+        hot: true
+	}).listen(9000, ipAddr, function(err) {
+		if(err) throw new gutil.PluginError("webpack-dev-server", err);
+		gutil.log("[webpack-dev-server]", "http://" + ipAddr + ":9000/webpack-dev-server/index.html");
+	});
+});
+
+gulp.task("build", function (callback) {
+    var myConfig = Object.create(webpackDev);
+    webpack(myConfig,function(err, stats) {
+        if(err) throw new gutil.PluginError("webpack-build", err);
+        gutil.log("[webpack:build]", stats.toString({
+            colors:true
+        }));
+        callback();
+    });
+});
+
+gulp.task("deploy", ["build"], function() {
+    ghpages.publish(path.join(__dirname,'..', 'build'), {
+        logger: function (msg) {
+            console.log(msg);
+        }
+    }, function(err) {
+            if (err) throw new gutil.PluginError("gh-pages", err);
+    });
+});
+
+
+// The development server (the recommended option for development)
+gulp.task("default", ["webpack-dev-server"]);
---
+++ app/templates/gulp/flatui.js
@@ -0,0 +1,125 @@
+var async = require('async');
+var del = require('del');
+var path = require('path');
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+var request = require('request');
+var zlib = require('zlib');
+var tar = require('tar');
+var gulp = require("gulp");
+var gutil = require("gulp-util");
+
+var BOOTSTRAP_VERSION = '3.3.1';
+var FLATUI_VERSION = '2.2.2';
+var latest_bootstrap_url = ['https://github.com/twbs/bootstrap/archive/', 'v', BOOTSTRAP_VERSION, '.tar.gz'].join('');
+var latest_flatui_url = ['https://github.com/designmodo/flat-ui/archive/', FLATUI_VERSION, '.tar.gz'].join('');
+var bootstrap_folder = ['bootstrap-', BOOTSTRAP_VERSION].join('');
+var flatui_folder = ['Flat-UI-', FLATUI_VERSION].join('');
+
+function getVendorFolder() {
+    return path.resolve(__dirname, '..', 'src', 'styles', 'vendor');
+}
+
+function renameBootstrapFolder() {
+    var vendorFolder = getVendorFolder();
+    fs.renameSync(
+        path.resolve(vendorFolder,bootstrap_folder),
+        path.resolve(vendorFolder, 'bootstrap')
+    );
+}
+
+function renameFlatUIFolder() {
+    var vendorFolder = getVendorFolder();
+    fs.renameSync(
+        path.resolve(vendorFolder,flatui_folder),
+        path.resolve(vendorFolder, 'flatui')
+    );
+}
+
+function getDownloadStream(url) {
+    return request.get(url);
+}
+
+function copyStyleFiles() {
+    var vendorFolder = getVendorFolder();
+    // copy glyphicons to fonts folder
+    var flatSrcGlob = [path.resolve(vendorFolder, 'flatui', 'fonts', 'glyphicons'), '/*'].join('');
+    var bootTargetFolder = path.resolve(vendorFolder, 'bootstrap', 'fonts', 'glyphicons');
+    gulp.src(flatSrcGlob)
+    .pipe(gulp.dest(bootTargetFolder));
+
+    // copy flatui fonts to its less folder
+    var flatFontsGlob = [path.resolve(vendorFolder, 'flatui', 'fonts'), '/**/*'].join('');
+    var flatLessTarget = [path.resolve(vendorFolder, 'flatui', 'less', 'fonts')].join('');
+    gulp.src(flatFontsGlob)
+        .pipe(gulp.dest(flatLessTarget));
+
+    // copy flatui img to its less folder
+    var flatImgGlob = [path.resolve(vendorFolder, 'flatui', 'img'), '/**/**/*'].join('');
+    var flatLessImgTarget = [path.resolve(vendorFolder, 'flatui', 'less', 'img')].join('');
+    gulp.src(flatImgGlob)
+        .pipe(gulp.dest(flatLessImgTarget));
+}
+
+gulp.task("clean:vendor", function() {
+    var vendorPath = getVendorFolder();
+    if (fs.existsSync(vendorPath)) {
+        del.sync(vendorPath)
+        gutil.log("deleted ", gutil.colors.yellow(vendorPath));
+    }
+});
+
+gulp.task("create:vendor", ["clean:vendor"], function() {
+    var vendorPath = getVendorFolder();
+    mkdirp.sync(vendorPath);
+    gutil.log(gutil.colors.yellow("created folder ", vendorPath));
+});
+
+gulp.task("setup:flatcss", ["create:vendor"], function() {
+    var path = getVendorFolder();
+    async.series({
+        bootstrap: function(callback) {
+            var bstream = getDownloadStream(latest_bootstrap_url);
+            bstream
+            .pipe(zlib.createGunzip())
+            .pipe(tar.Extract({path: path}))
+            .on('error', function(error) {
+                gutil.log(gutil.colors.read(error))
+                callback(error, null);
+            })
+            .on('end', function() {
+                gutil.log(gutil.colors.yellow("done download and extracting from ", latest_bootstrap_url));
+                callback(null, 'bootstrap');
+            });
+        },
+        flatui: function(callback) {
+            var fstream = getDownloadStream(latest_flatui_url);
+            fstream
+            .pipe(zlib.createGunzip())
+            .pipe(tar.Extract({path: path}))
+            .on('error', function(error) {
+                gutil.log(gutil.colors.read(error))
+                callback(error, null)
+            })
+            .on('end', function() {
+                gutil.log(gutil.colors.yellow("done download and extracting from ", latest_flatui_url));
+                callback(null, 'flatui');
+            });
+        }
+    }, function(error, results) {
+        if (error) {
+            gutil.log(gutil.colors.red(error));
+        }
+        else {
+            renameBootstrapFolder();
+            gutil.log(gutil.colors.yellow("done renaming bootstrap folder"));
+            renameFlatUIFolder();
+            gutil.log(gutil.colors.yellow("done renaming flat-ui folder"));
+            copyStyleFiles()
+            gutil.log(gutil.colors.yellow("copied style files from flatui to bootstrap"));
+        }
+    });
+
+});
+
+
