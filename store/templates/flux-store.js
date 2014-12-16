69309621956c9478f46d402ad4333e2859760195
Siddhartha Basu <biosidd@gmail.com>
2014-12-16 21:25:11 +0000

    Added template for flux store

---
+++ store/templates/flux-store.js
@@ -0,0 +1,34 @@
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+var ActionTypes = require('../constants/ActionTypes').ActionTypes;
+var assign = require('object-assign');
+var EventEmitter = require('events').EventEmitter;
+
+var CHANGE_EVENT = 'change';
+
+var <%= component %> = assign({}, EventEmitter.prototype, {
+    emitChange: function () {
+        this.emit(CHANGE_EVENT);
+    },
+    addChangeListener: function (callback) {
+        this.on(CHANGE_EVENT,callback);
+    },
+    removeChangeListener: function(callback) {
+        this.removeListener(CHANGE_EVENT, callback);
+    }
+
+    // add public API for store
+});
+
+AppDispatcher.register(function(payload) {
+    var action = payload.action;
+    switch(action.type) {
+        case ActionTypes.SOME_EVENT:
+            //update the store state or something else
+            <%= component %>.emitChange();
+        // Add more case statement for different action types
+        default:
+            //do nothing
+    }
+});
+
+module.exports = <%= component %>
