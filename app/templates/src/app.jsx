014b827059d500e4414f495e5b5c45a202e40140
Siddhartha Basu <biosidd@gmail.com>
2014-12-16 02:44:16 +0000

    Added instructions for including main stylesheet file

--- app/templates/src/app.jsx
+++ app/templates/src/app.jsx
@@ -2,9 +2,16 @@
  * Starting point of application
  */
 
+/*
+ * Before you uncomment the two lines below make sure you run
+ *  gulp setup:flatcss
+*/
+//require('../styles/main.less');
+//require('../styles/vendor/bootstrap/dist/js/bootstrap');
 
 var React = require('react');
 var router = require('./router');
+
 router.run(function (Handler) {
     React.render(<Handler/>, document.body);
 });
