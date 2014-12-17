1cfefaa1fe1778aa8bbf1cef9a021d2c890cf89b
Siddhartha Basu <biosidd@gmail.com>
2014-12-17 02:51:08 +0000

    added a method to normalize the generation of name

--- component/index.js
+++ component/index.js
@@ -6,21 +6,26 @@ module.exports = yeoman.generators.NamedBase.extend({
   initializing: function () {
     this.log(yosay(['Going to create component',  this.name ].join(' ')));
   },
-
+  normalizeName: function () {
+      if (this._.strRight(this.name, '-')) {
+          return this._.camelize(this.name);
+      }
+      return this._.capitalize(this.name);
+  },
   writing: {
     componentfile: function () {
-        var compName = this._.camelize(this.name);
+        var compName = this.normalizeName();
         this.fs.copyTpl(
-          'react-component.jsx',
-          ['src/scripts/components/', compName, '.jsx'].join(''),
+          this.templatePath('react-component.jsx'),
+          this.destinationPath(['src/scripts/components/', compName, '.jsx'].join('')),
           { component: compName }
         );
     },
     testfile: function () {
-        var compName = this._.camelize(this.name);
+        var compName = this.normalizeName();
         this.fs.copyTpl(
-          'react-component-test.js',
-          ['src/scripts/components/__tests__', compName, '-test.js'].join(''),
+          this.templatePath('react-component-test.js'),
+          this.destinationPath(['src/scripts/components/__tests__/', compName, '-test.js'].join('')),
           { component: compName }
         );
     }
--- store/index.js
+++ store/index.js
@@ -6,10 +6,15 @@ module.exports = yeoman.generators.NamedBase.extend({
   initializing: function () {
     this.log(yosay(['Going to create store',  this.name ].join(' ')));
   },
-
+  normalizeName: function () {
+      if (this._.strRight(this.name, '-')) {
+          return this._.camelize(this.name);
+      }
+      return this._.capitalize(this.name);
+  },
   writing: function () {
     storefile: function () {
-        var compName = this._.camelize([this.name, 'store'].join(''));
+        var compName = this.normalizeName();
         this.fs.copyTpl(
           this.templatePath('flux-store.js'),
           this.destinationPath(['src/scripts/stores/', compName, '.js'].join('')),
@@ -17,10 +22,10 @@ module.exports = yeoman.generators.NamedBase.extend({
         );
     },
     testfile: function () {
-        var compName = this._.camelize([this.name, 'store'].join(''));
+        var compName = this.normalizeName();
         this.fs.copyTpl(
           this.templatePath('flux-store-test.js'),
-          this.destinationPath(['src/scripts/stores/__tests__', compName, '-test.js'].join('')),
+          this.destinationPath(['src/scripts/stores/__tests__/', compName, '-test.js'].join('')),
           { component: compName }
         );
     }
